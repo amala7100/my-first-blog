1ae12dfc0d4605b448be12dff94d466e78b53f8a
amala7100 <amala7100@gmail.com>
2017-07-20 14:05:35 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%(xxzfw=z0ufwneg6hli%#n)^#98mzg5(exkh)hl0d$a5nn6v^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'http://amala.pythonanywhere.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'amala.pythonanywhere.com']
 
 
 # Application definition
